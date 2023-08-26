6ee00bb66ba090a077caa958b79a1cce41270da4
Carmartdev <amir8553mohammad@gmail.com>
2023-08-26 17:49:00 +0330

    Made app.js and style.css

---
+++ app.js
@@ -0,0 +1,17 @@
+var keysArray = [];
+document.addEventListener("keydown", function (event) {
+  var key = event.key;
+
+  if (key.match(/^[\x20-\x7E]$/)) {
+    // Check if the key pressed is a printable ASCII character
+    keysArray.push(key);
+  } else if (key.toLowerCase() === "backspace") {
+    keysArray.pop(); // remove last item from array if Backspace is pressed
+  }
+
+  // join array items into a single string
+  var keysString = keysArray.join("");
+
+  // display the resulting string in the h1 tag
+  document.querySelector("#keyText").textContent = keysString;
+});
